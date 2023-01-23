76b07d85051dd69df21875b30101c28068db2366
Marat Sayetgarayev <stgrmar@gmail.com>
2023-01-23 20:38:30 +0300

    сделал сложное дз

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ let num = 266219;
 let splitting = ('' + num).split('').map(Number)
 let multiply = splitting.reduce((a,b)=> a*b);
 let step = (multiply ** 3).toString();
-console.log(typeof (step))
 console.log(step.substring(0,2))

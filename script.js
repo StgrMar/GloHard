8fd55965c867dc260ebea462d1ed29334040dc7d
Marat Sayetgarayev <stgrmar@gmail.com>
2023-01-25 21:26:30 +0300

    сделал третье сложное

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-let num = 266219;
-let splitting = ('' + num).split('').map(Number)
-let multiply = splitting.reduce((a,b)=> a*b);
-let step = (multiply ** 3).toString();
-console.log(step.substring(0,2))
+'use strict'
+// lesson02
+// let num = 266219;
+// let splitting = ('' + num).split('').map(Number)
+// let multiply = splitting.reduce((a,b)=> a*b);
+// let step = (multiply ** 3).toString();
+// console.log(step.substring(0,2))
+
+
+//lesson03
+// let lang = confirm('Вывести дни на русском языке?');
+
+// if (lang == true)  {
+//     alert('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+// } else if (lang == false) {
+//     alert('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday')
+// };
+// console.log(lang)
+
+//  switch(lang) {
+//     case true:
+//         alert('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+//         break;
+    
+//     case false:
+//         alert('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday');
+//         break;
+//  };
+// console.log(lang);
+
+// let days = {
+//     true: ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
+//     false: ['Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'],
+// };
+
+// alert(days[lang]);
+// console.log(lang)
+
+// let namePerson = prompt('Введите имя', 'Артем');
+
+// let nameFromPrompt;
+// nameFromPrompt = namePerson === 'Артем' ? 'Директор' : namePerson === 'Алекс' ? 'Преподаватель' : 'студент';
+// alert(namePerson + " " + nameFromPrompt)
+
+
